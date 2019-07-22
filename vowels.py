372898b97a56108ae68dcb422d314d96195112a8
muthusrathinam <51087937+muthusrathinam@users.noreply.github.com>
2019-07-22 19:28:24 +0530

    Add files via upload

---
+++ vowels.py
@@ -0,0 +1,5 @@
+ch=input("enter an alphabet:")
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+     print("entered character is vowel.")
+else:
+     print("entered character is consonant")
