276b74549fe84d752f6152e2704941ec04c4f2db
muthusrathinam <51087937+muthusrathinam@users.noreply.github.com>
2019-07-23 18:45:50 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,6 @@
+n=int(input("enter no.:"))
+i=0
+while(n>0):
+     i=i+1
+     n=n//10
+print(i)
---
+++ sumn.py
@@ -0,0 +1,7 @@
+n=int(input("enter no.:"))
+sum=0
+for num in range(0,n+1,1):
+     sum=sum+num
+print(sum)
+     
+     
